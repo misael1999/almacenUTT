a9b67dc5c4bf5d81f16f63246efb1efe5abd7dad
Jesus Aguilar <jesus.aguilar.e.229@gmail.com>
2018-11-26 20:37:14 -0600

    Modificando Usuarios-sistema

--- src/app/pages/mantenimiento/usuarios/lista-usuarios/usuarios-sistema/usuarios-sistema.component.ts
+++ src/app/pages/mantenimiento/usuarios/lista-usuarios/usuarios-sistema/usuarios-sistema.component.ts
@@ -10,26 +10,7 @@ export class UsuariosSistemaComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-<<<<<<< HEAD
     //  this.store.dispatch(new fromUsuarios.LoadUsuarios());
-  }
-
-  public ordenaridUsuario() {
-    this.usuarios.sort((a, b) => {
-      const nameA = a.idUsuario;
-      const nameB = b.idUsuario;
-      if (nameA < nameB) {
-        return -1;
-      }
-      if (nameA > nameB) {
-        return 1;
-      }
-      return 0;
-    });
-
-    console.log(this.usuarios);
-=======
->>>>>>> f866b324ca0a6e6aae0e5fe13f6623f212f492a9
   }
 
 }
