bad83ff7d4eb939d82d4a53fa3d23d3a979c63d0
GerardCode <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2018-11-26 20:20:44 -0600

    Modificando componente descripcion-vale

--- src/app/pages/vales/descripcion-vale/descripcion-vale.component.ts
+++ src/app/pages/vales/descripcion-vale/descripcion-vale.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ValeSalida } from 'src/app/models/ValeSalida';
+import { AppState } from 'src/app/store/app.reducer';
+import { Store } from '@ngrx/store';
+import { ActivatedRoute } from '@angular/router';
+import * as fromValeSalida from 'src/app/store/actions';
 
 @Component({
   selector: 'app-descripcion-vale',
@@ -6,7 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DescripcionValeComponent implements OnInit {
 
-  constructor() { }
+  vale: ValeSalida;
+  loading: boolean;
+  loaded: boolean;
+  error: any[];
+
+  constructor(private store: Store<AppState>, private activatedRoute: ActivatedRoute) { 
+    this.activatedRoute.params.subscribe(params => {
+      const numero = params['numero'];
+      this.store.dispatch(new fromValeSalida.LoadValeSalida(numero));
+    });
+
+    this.store.select('vale')
+              .subscribe(vale => {
+                this.loading = vale.loading;
+                this.loaded = vale.loaded;
+                this.vale = vale.vale;
+                this.error = vale.error;
+              });
+  }
 
   ngOnInit() {
   }
