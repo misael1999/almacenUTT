f866b324ca0a6e6aae0e5fe13f6623f212f492a9
Jesus Aguilar <jesus.aguilar.e.229@gmail.com>
2018-11-26 18:24:54 -0600

    Revert "Tabla usuarios-area con sort"
    This reverts commit edd7bcee27d652dfec753696d83799e18355be5a.

--- src/app/pages/mantenimiento/usuarios/lista-usuarios/usuarios-sistema/usuarios-sistema.component.ts
+++ src/app/pages/mantenimiento/usuarios/lista-usuarios/usuarios-sistema/usuarios-sistema.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Usuario } from 'src/app/models/usuario';
-import { AppState } from 'src/app/store/app.reducer';
-import { Store } from '@ngrx/store';
-import * as fromUsuarios from '../../../../../store/actions';
 
 @Component({
   selector: 'app-usuarios-sistema',
@@ -11,38 +7,9 @@ import * as fromUsuarios from '../../../../../store/actions';
 })
 export class UsuariosSistemaComponent implements OnInit {
 
-  usuarios: Usuario[];
-  loading: boolean;
-  loaded: boolean;
-  error: any;
-
-  constructor(private store: Store<AppState>) {
-    this.store.select('usuarios').subscribe(usuarios => {
-      this.usuarios = usuarios.usuarios;
-      this.loaded = usuarios.loaded;
-      this.loading = usuarios.loading;
-      this.error = usuarios.error;
-    });
-   }
+  constructor() { }
 
   ngOnInit() {
-    // this.store.dispatch(new fromUsuarios.LoadUsuarios());
-  }
-
-  public ordenaridUsuario() {
-    this.usuarios.sort((a, b) => {
-      const nameA = a.idUsuario;
-      const nameB = b.idUsuario;
-      if (nameA < nameB) {
-        return -1;
-      }
-      if (nameA > nameB) {
-        return 1;
-      }
-      return 0;
-    });
-
-    console.log(this.usuarios);
   }
 
 }
